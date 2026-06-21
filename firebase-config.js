094e11f714a8062f4c9533468a1d6d155e076909
gyouhoufestival_nagano <kirakirasub0328@gmail.com>
2026-06-21 20:52:08 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAAlxZGPH72mvgePyM3RX_xy8xlhfv92Rk",
+  authDomain: "yoshidagyouhoufestival1-5.firebaseapp.com",
+  databaseURL: "https://yoshidagyouhoufestival1-5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "yoshidagyouhoufestival1-5",
+  storageBucket: "yoshidagyouhoufestival1-5.firebasestorage.app",
+  messagingSenderId: "439419753147",
+  appId: "1:439419753147:web:81c755345bbca47b2f0f9e"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
